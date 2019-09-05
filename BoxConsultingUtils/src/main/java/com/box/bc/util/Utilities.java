81c78dffaa0bb229785d64ddde284b39006b0e6b
pmatte1 <pmatte+github@box.com>
2019-09-05 09:50:31 -0400

    Updated getIdFrom409Exception logic to return values

--- BoxConsultingUtils/src/main/java/com/box/bc/util/Utilities.java
+++ BoxConsultingUtils/src/main/java/com/box/bc/util/Utilities.java
@@ -3,15 +3,19 @@ package com.box.bc.util;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
 
+import com.box.sdk.BoxAPIException;
+
 public class Utilities {
 	public static Logger logger = Logger.getLogger(Utilities.class);
 
 	public static String getIdFrom409Exception(BoxAPIException e){
 		if(e.getResponseCode() == 409){
 			//Get the ID from the response and get folder
-			logger.info("Folder with the name " + getFolderName() + " already exists.  Configuring existing folder...");
+			logger.debug("Conflict Error: " + e.getResponse());
 			JSONObject responseJson = new JSONObject(e.getResponse());
-			String id = responseJson.getJSONObject("context_info").getJSONArray("conflicts").getJSONObject(0).getString("id");
+			return responseJson.getJSONObject("context_info").getJSONArray("conflicts").getJSONObject(0).getString("id");
 		}
+		
+		return null;
 	}
 }
